50fa92d70bf714917668979d9df10eb23d376d8f
Franka <fgriem@gmail.com>
2026-06-24 15:35:52 +0200

    display sidebar with project links

--- src/Project.js
+++ src/Project.js
@@ -3,6 +3,7 @@ import createTodo from "./Todo.js";
 export default class Project {
 
     title;
+    id = crypto.randomUUID();
     description;
     todoList = [];
 
@@ -11,13 +12,21 @@ export default class Project {
         this.description = description;
     }
 
+    get todos(){
+        return this.todoList;
+    }
+
+    get title(){
+        return this.title;
+    }
+
+    get id(){
+        return this.id;
+    }
+
     addTodo(title, description, dueDate, priority){
         this.todoList.push(createTodo(title, description, dueDate, priority));
         console.log(this.todoList);
-    }
-
-    getTodos(){
-        return this.todoList;
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import '../node_modules/modern-normalize/modern-normalize.css';
 import "./style.css";
 import Project from './Project.js';
+import displayProjects from './displayProjects.js';
+
+let projects = [];
 
 const myDefaultProject = new Project("My To Dos", "just the default project");
 var tomorrow = new Date();
 tomorrow.setDate(tomorrow.getDate() + 1);
 myDefaultProject.addTodo("do the dishes", "just do it, ok", tomorrow);
+projects.push(myDefaultProject);
 
-console.log(myDefaultProject);
-console.log("hello world!");
+displayProjects(projects);
+
+console.log(projects);
